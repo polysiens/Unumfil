2fddec5808667b2a3b2a4347ebda4f60bfe0c782
dejanzelenjak <dejan.zelenjak.business@gmail.com>
2023-09-23 17:42:31 +0200

    Added structure for Bison Wrapper

---
+++ Source/include/BisonWrap.h
@@ -0,0 +1,15 @@
+#ifndef BISONWRAP_HEADER_GUARD
+#define BISONWRAP_HEADER_GUARD
+
+/* [Includes] */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* [Definitions] */
+//
+
+/* [Interface Functions] */
+int main();
+
+
+#endif //#BISONWRAP_HEADER_GUARD
---
+++ Source/src/BisonWrap.c
@@ -0,0 +1,16 @@
+/* [Includes] */
+#include "../include/BisonWrap.h"
+
+/* [Definitions] */
+//
+
+/* [Static Functions] */
+//
+
+/* Code Section */
+int main()
+{
+    printf("Hello World!");
+
+    return 0;
+}
