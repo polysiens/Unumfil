9c4148919ddd53a1efa1cf966343147f35da8506
Polysiens <polysiens@gmail.com>
2023-09-24 18:26:45 +0200

    I added IT to ignore

--- Dependencies/Libraries/bin/Build_Lexer.bat
+++ Dependencies/Libraries/bin/Build_Lexer.bat
@@ -2,12 +2,10 @@
 
 @echo off
 
-set PATH_TO_COMPILER="C:\msys64\mingw64\bin\gcc.exe"
-set PATH_TO_SOURCE="D:\GIT\MultiThreadLanguageProject\Dependencies\Libraries\bin\lex.yy.c"
+set PATH_TO_SOURCE="E:\Work\Personal\MultiThreadLanguageProject\Dependencies\Libraries\bin\lex.yy.c"
 
-@echo "Compiler: %PATH_TO_COMPILER%"
-@echo .
 @echo "Source: %PATH_TO_SOURCE%"
+@echo .
 
 :: Uncomment this code and put the drive letter of the drive where your compiler is
 :: If the compiler and source aren't on the same drive.
@@ -15,7 +13,7 @@ set PATH_TO_SOURCE="D:\GIT\MultiThreadLanguageProject\Dependencies\Libraries\bin
 
 @echo "Calling gcc to compile the lexer.."
 @echo .
-%PATH_TO_COMPILER% -o lexer.exe %PATH_TO_SOURCE% -I.
+gcc -o lexer.exe %PATH_TO_SOURCE% -I.
 ::This is a blatant lie :)
 @echo "Lexer successfully compiled!"
 pause
